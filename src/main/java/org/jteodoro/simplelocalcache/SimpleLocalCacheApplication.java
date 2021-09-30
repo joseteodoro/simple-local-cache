85ff93f52157f1826b71501ced37552e20fe655c
joseteodoro <jteodoro.dev@gmail.com>
2021-09-30 10:06:07 -0300

    adding basic maven project

---
+++ src/main/java/org/jteodoro/simplelocalcache/SimpleLocalCacheApplication.java
@@ -0,0 +1,13 @@
+package org.jteodoro.simplelocalcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLocalCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLocalCacheApplication.class, args);
+	}
+
+}
